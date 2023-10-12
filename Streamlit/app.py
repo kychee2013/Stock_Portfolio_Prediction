42a1c48314eaca99d4b8b21e1a1f48caa63bac35
kychee2013 <132038948+kychee2013@users.noreply.github.com>
2023-10-12 16:51:14 +0800

    Update app.py

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -61,7 +61,7 @@ with st.spinner("Loading, please wait..."):
     flag = df.drop(['Company Name', 'Sector'], axis=1).T.sum().sort_values(ascending=False).index[:10]
     df = df.loc[flag]
     df = pd.concat([df, ytd_df.set_index("Ticker")["YTD Performance"]], axis=1)
-    df.rename(columns={"YTD Performance":"YTD Performance (%)"})
+    df.rename(columns={"YTD Performance":"YTD Performance (%)"}, inplace=True)
     df["YTD Performance (%)"].astype(int)
     #df["YTD Performance (%)"].astype(int)
     # df["YTD Performance"] = [get_ytd_performance(ticker) for ticker in flag]
